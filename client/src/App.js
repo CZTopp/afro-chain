8840de29ce73b711ee73675aaee297ec083ce01a
AlQalamX <phmgllc@gmail.com>
2019-06-10 21:21:09 -0400

    clean up add border to blocks component

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,13 @@ import './App.css';
 import Header from './components/Header/Header';
 import PageBlocks from './components/PageBlocks/PageBlocks';
 import Footer from './components/Footer/Footer';
+import DataCenter from './components/DataCenter/DataCenter';
+
 function App() {
   return (
     <div className="App">
       <Header />
+      <DataCenter/>
       <PageBlocks />
       <Footer />
     </div>
